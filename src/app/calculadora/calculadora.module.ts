d20fac071965d0f395eb50f9fdbab87be9ef0f3a
taisbelucik <taisbelucik@hotmail.com>
2023-06-25 21:03:36 -0300

    criando módulo, componente e serviço calculadora

--- src/app/calculadora/calculadora.module.ts
+++ src/app/calculadora/calculadora.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CalculadoraComponent } from './componentes';
+import { CalculadoraService } from './services';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CalculadoraComponent
+  ],
   imports: [
     CommonModule
+  ],
+
+  exports: [
+   CalculadoraComponent
+  ],
+
+  providers: [
+  CalculadoraService
   ]
+
 })
 export class CalculadoraModule { }
